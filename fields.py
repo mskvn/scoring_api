9f1dbbdd45da020465ffc2015d93349c892c6366
Aleksei Moskvin <a.moskvin@propellerads.net>
2020-07-06 21:50:43 +0300

    refactor fields

--- fields.py
+++ fields.py
@@ -14,6 +14,10 @@ class Field:
     def __get__(self, instance, cls):
         return instance.__dict__.get(self.name)
 
+    def __set__(self, instance, value):
+        self._validate(value)
+        instance.__dict__[self.name] = value
+
     def _validate(self, value):
         if self.required and value is None:
             raise ValidationError(f'{self.name} is required')
@@ -22,111 +26,93 @@ class Field:
 
 
 class CharField(Field):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
-
-    def __set__(self, instance, value):
-        super()._validate(value)
-        if value:
-            if not isinstance(value, str):
-                raise ValidationError(f'{self.name} must be a str')
-        instance.__dict__[self.name] = value
-
-
-class ArgumentsField(Field):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
-
-    def __set__(self, instance, value):
-        super()._validate(value)
-        if value:
-            if not isinstance(value, dict):
-                raise ValidationError(f'{self.name} must be a dict')
-        instance.__dict__[self.name] = value
-
-
-class EmailField(CharField):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
-
-    def __set__(self, instance, value):
-        super()._validate(value)
-        if value:
-            if not isinstance(value, str):
-                raise ValidationError(f'{self.name} must be a str')
-            if '@' not in value:
-                raise ValidationError(f'{self.name} must contain @')
-        instance.__dict__[self.name] = value
-
-
-class PhoneField(Field):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
-
-    def __set__(self, instance, value):
-        if value:
-            if not isinstance(value, str) and not isinstance(value, int):
-                raise ValidationError(f'{self.name} must be a str or int')
-            if len(str(value)) != 11:
-                raise ValidationError(f'{self.name} length should equal 11')
-            if not str(value).startswith('7'):
-                raise ValidationError(f'{self.name} should start with 7')
-        instance.__dict__[self.name] = value
-
-
-class DateField(Field):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
 
     def _validate(self, value):
         super()._validate(value)
         if value:
-            try:
-                return datetime.datetime.strptime(value, '%d.%m.%Y')
-            except ValueError:
-                raise ValidationError(f"Incorrect date format of {self.name}, should be DD.MM.YYYY")
+            if not isinstance(value, str):
+                raise ValidationError(f'{self.name} must be a str')
 
-    def __set__(self, instance, value):
-        date = self._validate(value)
-        instance.__dict__[self.name] = date
+
+class ArgumentsField(Field):
+
+    def _validate(self, value):
+        super()._validate(value)
+        if not value:
+            return
+        if not isinstance(value, dict):
+            raise ValidationError(f'{self.name} must be a dict')
+
+
+class EmailField(CharField):
+
+    def _validate(self, value):
+        super()._validate(value)
+        if not value:
+            return
+        if not isinstance(value, str):
+            raise ValidationError(f'{self.name} must be a str')
+        if '@' not in value:
+            raise ValidationError(f'{self.name} must contain @')
+
+
+class PhoneField(Field):
+
+    def _validate(self, value):
+        super()._validate(value)
+        if not value:
+            return
+        if not isinstance(value, str) and not isinstance(value, int):
+            raise ValidationError(f'{self.name} must be a str or int')
+        if len(str(value)) != 11:
+            raise ValidationError(f'{self.name} length should equal 11')
+        if not str(value).startswith('7'):
+            raise ValidationError(f'{self.name} should start with 7')
+
+
+class DateField(Field):
+    date_format = '%d.%m.%Y'
+
+    def _validate(self, value):
+        super()._validate(value)
+        if not value:
+            return
+        try:
+            datetime.datetime.strptime(value, self.date_format)
+        except ValueError:
+            raise ValidationError(f"Incorrect date format of {self.name}, should be DD.MM.YYYY")
 
 
 class BirthDayField(DateField):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
 
-    def __set__(self, instance, value):
-        date = super()._validate(value)
-        if date:
-            min_year = datetime.datetime.now().year - 70
-            if date.year < min_year:
-                raise ValidationError(f"Incorrect {self.name}: year should be more then {min_year}")
-        instance.__dict__[self.name] = date
+    def _validate(self, value):
+        super()._validate(value)
+        if not value:
+            return
+        date = datetime.datetime.strptime(value, self.date_format)
+        min_year = datetime.datetime.now().year - 70
+        if date.year < min_year:
+            raise ValidationError(f"Incorrect {self.name}: year should be more then {min_year}")
 
 
 class GenderField(Field):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
 
-    def __set__(self, instance, value):
+    def _validate(self, value):
         super()._validate(value)
-        if value:
-            valid_values = [0, 1, 2]
-            if value not in valid_values:
-                raise ValidationError(f'Incorrect {self.name}. Valid values: {valid_values}')
-        instance.__dict__[self.name] = value
+        if not value:
+            return
+        valid_values = [0, 1, 2]
+        if value not in valid_values:
+            raise ValidationError(f'Incorrect {self.name}. Valid values: {valid_values}')
 
 
 class ClientIDsField(Field):
-    def __init__(self, required, nullable):
-        super().__init__(required, nullable)
 
-    def __set__(self, instance, value):
+    def _validate(self, value):
         super()._validate(value)
-        if value:
-            if not isinstance(value, list):
-                raise ValidationError(f'{self.name} must be a list')
-            else:
-                if not all(isinstance(x, int) for x in value):
-                    raise ValidationError(f'All values from {self.name} should be integers')
-        instance.__dict__[self.name] = value
+        if not value:
+            return
+        if not isinstance(value, list):
+            raise ValidationError(f'{self.name} must be a list')
+        if not all(isinstance(x, int) for x in value):
+            raise ValidationError(f'All values from {self.name} should be integers')
