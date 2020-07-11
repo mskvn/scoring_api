790a0efc77c778208d53cb0d526eb3f94c97204d
Aleksei Moskvin <a.moskvin@propellerads.net>
2020-07-11 17:09:52 +0300

    add store

---
+++ store.py
@@ -0,0 +1,14 @@
+import redis
+
+
+class Store:
+    SOCKET_TIMEOUT = 5
+
+    def __init__(self, host='localhost', port=6379):
+        self._client = redis.Redis(host=host, port=port, socket_timeout=self.SOCKET_TIMEOUT)
+
+    def get(self, name):
+        return self._client.get(name)
+
+    def cache_set(self, key, value, ttl):
+        self._client.set(name=key, value=value, ex=ttl)
