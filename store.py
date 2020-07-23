792830a39b1e20ec3066bec1d33996a4cf99e22f
Aleksei Moskvin <a.moskvin@propellerads.net>
2020-07-23 20:19:18 +0300

    added retries in store

--- store.py
+++ store.py
@@ -1,8 +1,28 @@
 import logging
+import time
+from functools import wraps
 
 import redis
+from redis.exceptions import TimeoutError, ConnectionError
 
 from exceptions import StoreGetException
+
+
+def retry(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        tries, delay = 3, 0.5
+        while tries > 0:
+            try:
+                return f(*args, **kwargs)
+            except (TimeoutError, ConnectionError) as e:
+                msg = '{}, Retrying in {} seconds...'.format(e, delay)
+                logging.info(msg)
+                time.sleep(delay)
+                tries -= 1
+        return f(*args, **kwargs)
+
+    return wrapper
 
 
 class Store:
@@ -22,13 +42,21 @@ class Store:
 
     def cache_get(self, name):
         try:
-            return self._client.get(name)
+            return self.get_with_retry(name)
         except Exception as e:
             logging.error(str(e))
             return None
 
     def cache_set(self, key, value, ttl=None):
         try:
-            self._client.set(name=key, value=value, ex=ttl)
+            self.set_with_retry(key, value, ttl)
         except Exception as e:
             logging.error(str(e))
+
+    @retry
+    def set_with_retry(self, key, value, ttl=None):
+        self._client.set(name=key, value=value, ex=ttl)
+
+    @retry
+    def get_with_retry(self, name):
+        return self._client.get(name)
